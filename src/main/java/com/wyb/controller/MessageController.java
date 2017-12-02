f30d2b83f851d1a5671abe6538385840104b7f2f
stagebo <1254117589@qq.com>
2017-12-02 15:21:32 +0800

    2017-12-2 15:21:24

--- src/main/java/com/wyb/controller/MessageController.java
+++ src/main/java/com/wyb/controller/MessageController.java
@@ -1,34 +1,73 @@
 package com.wyb.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hibernate.HibernateSessionFactory;
-
 
 @Controller
 @RequestMapping(value = "Message")
 public class MessageController {
 
 	@RequestMapping(value = "GetMessagePage")
+	@ResponseBody
 	public String toLogin(HttpServletRequest request,
 			HttpServletResponse response) {
-
+		String savePath = request.getSession().getServletContext()
+				.getRealPath("/")
+				+ "Files\\";
+		System.out.println(savePath);
 		return "123";
 
+	}
+
+	@RequestMapping(value = "excuteSql")
+	@ResponseBody
+	public String excuteSql(HttpServletRequest request,
+			HttpServletResponse response) {
+		String sql = request.getParameter("sql");
+
+		try {
+			Session session = HibernateSessionFactory.getSession();
+			Transaction tx = session.beginTransaction();
+
+			SQLQuery query = session.createSQLQuery(sql);
+			int t = query.executeUpdate();
+
+			tx.commit();
+			return t + "";
+		} catch (Exception ex) {
+
+			return "0";
+		}
 	}
 
 	@RequestMapping(value = "getTimes")
@@ -39,49 +78,49 @@ public class MessageController {
 		String cna = request.getParameter("cna");
 		String cpno = request.getParameter("cpno");
 		String cc = request.getParameter("cc");
-		
-		
+
 		Session session = HibernateSessionFactory.getSession();
 		Transaction tx = session.beginTransaction();
-		String sql = "insert into course values('"+cno+"','"+cna+"','"+cpno+"','"+cc+"')";
+		String sql = "insert into course values('" + cno + "','" + cna + "','"
+				+ cpno + "','" + cc + "')";
 		SQLQuery query = session.createSQLQuery(sql);
 		int t = query.executeUpdate();
-		
+
 		tx.commit();
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//
 
 		String time = df.format(new Date());// new Date()
-		return "添加结果：" + t + "!";
+		return t + "";
 	}
-	
+
 	@RequestMapping(value = "getCourse")
 	@ResponseBody
 	public String getCourse() {
 		Session session = HibernateSessionFactory.getSession();
-		
+
 		String sql = "select * from course";
-		
-		//session.createSQLQuery(sql).list(); 
+
+		// session.createSQLQuery(sql).list();
 		SQLQuery query = session.createSQLQuery(sql);
-		List<Object[]> cl =query.list();
-		
+		List<Object[]> cl = query.list();
+
 		StringBuilder sb = new StringBuilder();
 		sb.append("[");
 		boolean isStart = true;
-		for(Object[] o :cl){
-			
+		for (Object[] o : cl) {
+
 			String cno = o[0].toString();
 			String cna = o[1].toString();
 			String cpno = o[2].toString();
 			String cc = o[3].toString();
-			
-			sb.append(isStart?"":",");
+
+			sb.append(isStart ? "" : ",");
 			isStart = false;
 			sb.append("{");
-			sb.append("\"cno\":\""+cno+"\",");
-			sb.append("\"cna\":\""+cna+"\",");
-			sb.append("\"cpno\":\""+cpno+"\",");
-			sb.append("\"cc\":\""+cc+"\"");
+			sb.append("\"cno\":\"" + cno + "\",");
+			sb.append("\"cna\":\"" + cna + "\",");
+			sb.append("\"cpno\":\"" + cpno + "\",");
+			sb.append("\"cc\":\"" + cc + "\"");
 			sb.append("}");
 		}
 		sb.append("]");
@@ -90,37 +129,37 @@ public class MessageController {
 		String time = df.format(new Date());// new Date()
 		return sb.toString();
 	}
-	
+
 	@RequestMapping(value = "getStudent")
 	@ResponseBody
 	public String getStudent() {
 		Session session = HibernateSessionFactory.getSession();
-		
+
 		String sql = "select * from student";
-		
-		//session.createSQLQuery(sql).list(); 
+
+		// session.createSQLQuery(sql).list();
 		SQLQuery query = session.createSQLQuery(sql);
-		List<Object[]> cl =query.list();
-		
+		List<Object[]> cl = query.list();
+
 		StringBuilder sb = new StringBuilder();
 		sb.append("[");
 		boolean isStart = true;
-		for(Object[] o :cl){
-			
+		for (Object[] o : cl) {
+
 			String sno = o[0].toString();
 			String cna = o[1].toString();
 			String csex = o[2].toString();
 			String sage = o[3].toString();
 			String sdept = o[4].toString();
-			
-			sb.append(isStart?"":",");
+
+			sb.append(isStart ? "" : ",");
 			isStart = false;
 			sb.append("{");
-			sb.append("\"sno\":\""+sno+"\",");
-			sb.append("\"sna\":\""+cna+"\",");
-			sb.append("\"ssex\":\""+csex+"\",");
-			sb.append("\"sage\":\""+sage+"\",");
-			sb.append("\"sdept\":\""+sdept+"\"");
+			sb.append("\"sno\":\"" + sno + "\",");
+			sb.append("\"sna\":\"" + cna + "\",");
+			sb.append("\"ssex\":\"" + csex + "\",");
+			sb.append("\"sage\":\"" + sage + "\",");
+			sb.append("\"sdept\":\"" + sdept + "\"");
 			sb.append("}");
 		}
 		sb.append("]");
@@ -129,33 +168,33 @@ public class MessageController {
 		String time = df.format(new Date());// new Date()
 		return sb.toString();
 	}
-	
+
 	@RequestMapping(value = "getSC")
 	@ResponseBody
 	public String getSC() {
 		Session session = HibernateSessionFactory.getSession();
-		
+
 		String sql = "select * from sc";
-		
-		//session.createSQLQuery(sql).list(); 
+
+		// session.createSQLQuery(sql).list();
 		SQLQuery query = session.createSQLQuery(sql);
-		List<Object[]> cl =query.list();
-		
+		List<Object[]> cl = query.list();
+
 		StringBuilder sb = new StringBuilder();
 		sb.append("[");
 		boolean isStart = true;
-		for(Object[] o :cl){
-			
+		for (Object[] o : cl) {
+
 			String sno = o[0].toString();
 			String cno = o[1].toString();
 			String grade = o[2].toString();
-			
-			sb.append(isStart?"":",");
+
+			sb.append(isStart ? "" : ",");
 			isStart = false;
 			sb.append("{");
-			sb.append("\"sno\":\""+sno+"\",");
-			sb.append("\"cno\":\""+cno+"\",");
-			sb.append("\"grade\":\""+grade+"\"");
+			sb.append("\"sno\":\"" + sno + "\",");
+			sb.append("\"cno\":\"" + cno + "\",");
+			sb.append("\"grade\":\"" + grade + "\"");
 			sb.append("}");
 		}
 		sb.append("]");
@@ -164,4 +203,90 @@ public class MessageController {
 		String time = df.format(new Date());// new Date()
 		return sb.toString();
 	}
+
+	@RequestMapping("springUpload")
+	public String springUpload(HttpServletRequest request)
+			throws IllegalStateException, IOException {
+		String savePath = request.getSession().getServletContext()
+				.getRealPath("/")
+				+ "WEB-INF\\static\\imgFiles\\";
+
+		String message = "上传成功";
+
+		String uuid = UUID.randomUUID().toString();
+		String fileName = savePath + uuid + ".jpg";
+		long startTime = System.currentTimeMillis();
+		// 将当前上下文初始化给 CommonsMutipartResolver （多部分解析器）
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+				request.getSession().getServletContext());
+		// 检查form中是否有enctype="multipart/form-data"
+		if (multipartResolver.isMultipart(request)) {
+			// 将request变成多部分request
+			MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+			// 获取multiRequest 中所有的文件名
+			Iterator iter = multiRequest.getFileNames();
+
+			while (iter.hasNext()) {
+				// 一次遍历所有文件
+				MultipartFile file = multiRequest.getFile(iter.next()
+						.toString());
+				if (file != null) {
+					// 上传
+					file.transferTo(new File(fileName));
+					String sql = "insert into image values('static/imgFiles/"
+							+ uuid + ".jpg')";
+
+					try {
+						Session session = HibernateSessionFactory.getSession();
+						Transaction tx = session.beginTransaction();
+
+						SQLQuery query = session.createSQLQuery(sql);
+						int t = query.executeUpdate();
+
+						tx.commit();
+
+					} catch (Exception ex) {
+
+						message = "存入数据库失败！";
+					}
+				}
+
+			}
+
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("方法三的运行时间：" + String.valueOf(endTime - startTime)
+				+ "ms");
+
+		return "redirect:/index.jsp";
+	}
+
+	@RequestMapping(value = "getImgList")
+	@ResponseBody
+	public String getImgList() {
+		Session session = HibernateSessionFactory.getSession();
+
+		String sql = "select filename,filename as f1 from image";
+
+		// session.createSQLQuery(sql).list();
+		SQLQuery query = session.createSQLQuery(sql);
+		List<Object[]> cl = query.list();
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		boolean isStart = true;
+		for (Object[] o : cl) {
+			String src = o[0].toString();
+
+			sb.append(isStart ? "" : ",");
+			isStart = false;
+			sb.append("{");
+			sb.append("\"src\":\"" + src + "\"");
+
+			sb.append("}");
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -13,96 +13,231 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
-<link href="static/plugins/MiniUI/themes/default/miniui.css">
-<link rel="stylesheet" type="text/css" href="static/plugins/MiniUI/themes/default/miniui.css" />
+
+<link rel="stylesheet" type="text/css"
+	href="static/plugins/MiniUI/themes/default/miniui.css" />
+<link rel="stylesheet" type="text/css"
+	href="static/plugins/Bootstrap/css/bootstrap-theme.css" />
+<link rel="stylesheet" type="text/css"
+	href="static/plugins/Bootstrap/css/bootstrap.css" />
 <script src="static/plugins/jquery/jquery-1.12.2.js"></script>
 <script src="static/plugins/MiniUI/miniui.js"></script>
-<script>
-	$(function() {
+<script src="static/plugins/Bootstrap/js/bootstrap.min.js"></script>
+<style>
+.tab-item {
+	width: 80%;
+	margin-left: 10%;
+	border: 1px solid;
+	padding-left: 20px;
+}
 
-		$("#sub").click(function() {
-			var cno = $("#no").val();
-			var cna = $("#na").val();
-			var cpno = $("#pno").val();
-			var cc = $("#cc").val();
-			var opt = {
-				cno : cno,
-				cna : cna,
-				cpno : cpno,
-				cc : cc
-
-			};
-			$.ajax({
-				url : '/DatabaseCourse' + '/Message/getTimes',
-				type : 'get',
-				data : opt,
-				dataType : 'json',
-				success : function(d) {
-					alert(d);
-				}
-			});
-
-		});
-	});
-</script>
+img {
+	width: 150px;
+	height: 150px;
+	float: left;
+	margin-left: 5px;
+	margin-top: 5px;
+}
+</style>
 </head>
 <body>
-	<%-- <%response.sendRedirect("/spring_mvc01/toLogin");%> --%>
-	<br> CNO：
-	<input type="input" id="no" />
-	<br>
-	<br> CNAME：
-	<input type="input" id="na" />
-	<br>
-	<br> CPNO：
-	<input type="input" id="pno" />
-	<br>
-	<br> CCREDIT：
-	<input type="input" id="cc" />
-	<br>
-	<br>
-	<input type="button" value="添加" id="sub" />
-	<br>
-	<br>
-	<div>
-		<input type="button" value="查询" id="search" />
+	<div class="tab-item">
+		<h1>添加学生</h1>
+
+		<br> <span>sno：</span> <input type="input" id="s_no" /> <br>
+		<br> <span>sname：</span> <input type="input" id="s_na" /> <br>
+		<br> <span>ssex：</span> <input type="input" id="s_sex" /> <br>
+		<br> <span>sage：</span> <input type="input" id="s_age" /> <br>
+		<br> <span>sdept：</span> <input type="input" id="s_dept" /> <br>
+		<button type="button" class="btn btn-primary" id="sub_add_stu">添加学生</button>
+		<script>
+			$(function() {
+				$("#sub_add_stu").click(
+						function() {
+							var no = $("#s_no").val();
+							var na = $("#s_na").val();
+							var sex = $("#s_sex").val();
+							var age = $("#s_age").val();
+							var dept = $("#s_dept").val();
+							var sql = "insert into student values ('" + no
+									+ "','" + na + "','" + sex + "','" + age
+									+ "','" + dept + "')";
+							var opt = {
+								sql : sql
+							};
+							$.ajax({
+								url : '/DatabaseCourse' + '/Message/excuteSql',
+								type : 'get',
+								data : opt,
+								dataType : 'json',
+								success : function(d) {
+									alert(d);
+								}
+							});
+
+						});
+			});
+		</script>
 
 
-		<div id="datagrid1" class="mini-datagrid"
-			style="width:700px;height:250px;">
-			<div property="columns">
-				<div type="indexcolumn"></div>
+		<h1>查找学生</h1>
+		<div>
+			<button type="button" class="btn btn-primary" id="search_stu">查询学生</button>
 
-				<div field="cno" width="120">CNO</div>
-				<div field="cna" width="100">CNAME</div>
-				<div field="cpno" width="100" allowSort="true">CPNO</div>
-				<div field="cc" width="100" allowSort="true">CCredit</div>
+			<div id="datagrid2" class="mini-datagrid"
+				style="width:700px;height:250px;">
+				<div property="columns">
+					<div type="indexcolumn"></div>
+					<div field="sno" width="120">SNO</div>
+					<div field="sna" width="120">SNAME</div>
+					<div field="ssex" width="100">SSEX</div>
+					<div field="sage" width="100">SAGE</div>
+					<div field="sdept" width="100">SDEPT</div>
+				</div>
 			</div>
 		</div>
+		<script type="text/javascript">
+			mini.parse();
+			var grid = mini.get("datagrid2");
+			$(function() {
+
+				$("#search_stu").click(function() {
+					$.ajax({
+						url : '/DatabaseCourse' + '/Message/getStudent',
+						type : 'get',
+						data : '',
+						dataType : 'json',
+						success : function(data) {
+							console.log(data);
+							mini.get("datagrid2").setData(data);
+
+						}
+
+					});
+
+				});
+			})
+		</script>
+
 	</div>
-<script type="text/javascript">
-mini.parse();
-var grid = mini.get("datagrid1");
-$(function(){
-	
-	$("#search").click(function(){
-		$.ajax({
-			url:'/DatabaseCourse' + '/Message/getCourse',
-			type:'get',
-			data:'',
-			dataType:'json',
-			success:function(data){
-				grid.setData(data);
-				
-			}
-			
-		});
-		
-	});
-})
+	<div class="tab-item">
+		<h1>添加课程</h1>
+		<br> <span>CNO：</span> <input type="input" id="no" /> <br>
+		<br> <span>CNAME：</span> <input type="input" id="na" /> <br>
+		<br> <span>CPNO：</span> <input type="input" id="pno" /> <br>
+		<br> <span>CCREDIT：</span> <input type="input" id="cc" /> <br>
+		<br>
+		<button type="button" class="btn btn-primary" id="sub">添加课程</button>
+		<br> <br>
+		<script>
+			$(function() {
+				$("#sub").click(function() {
+					var cno = $("#no").val();
+					var cna = $("#na").val();
+					var cpno = $("#pno").val();
+					var cc = $("#cc").val();
+					var opt = {
+						cno : cno,
+						cna : cna,
+						cpno : cpno,
+						cc : cc
 
-</script>
+					};
+					$.ajax({
+						url : '/DatabaseCourse' + '/Message/getTimes',
+						type : 'get',
+						data : opt,
+						dataType : 'json',
+						success : function(d) {
+							alert(d);
+						}
+					});
 
+				});
+			});
+		</script>
+		<h1>查找课程</h1>
+		<div>
+			<button type="button" class="btn btn-primary" id="search">查询课程</button>
+
+			<div id="datagrid1" class="mini-datagrid"
+				style="width:700px;height:250px;">
+				<div property="columns">
+					<div type="indexcolumn"></div>
+
+					<div field="cno" width="120">CNO</div>
+					<div field="cna" width="100">CNAME</div>
+					<div field="cpno" width="100" allowSort="true">CPNO</div>
+					<div field="cc" width="100" allowSort="true">CCredit</div>
+				</div>
+			</div>
+		</div>
+		<script type="text/javascript">
+			mini.parse();
+			var grid = mini.get("datagrid1");
+			$(function() {
+
+				$("#search").click(function() {
+					$.ajax({
+						url : '/DatabaseCourse' + '/Message/getCourse',
+						type : 'get',
+						data : '',
+						dataType : 'json',
+						success : function(data) {
+							grid.setData(data);
+
+						}
+
+					});
+
+				});
+			})
+		</script>
+
+
+
+	</div>
+
+	<div class="tab-item">
+		<h1>上传图片</h1>
+		<div>
+			<form action="<%=basePath%>/Message/springUpload" method="post"
+				enctype="multipart/form-data">
+				<input type="file" value="请选择图片" name="imgfile" /> <input
+					type="submit" value="上传" />
+			</form>
+		</div>
+
+		<div id="imgcont"
+			style="width:100%;height:500px;background-color:#aaa;">
+
+			<!-- <img src="static/imgFiles/9a5cc4ec-a4da-427e-a7b2-bd72c4f32744.jpg"/> -->
+
+		</div>
+		<script>
+			$(function() {
+
+				$.ajax({
+					url : '/DatabaseCourse' + '/Message/getImgList',
+					type : 'get',
+					data : '',
+					dataType : 'json',
+					success : function(data) {
+
+						$("#imgcont").html("");
+						console.log(data);
+						data.forEach(function(v) {
+							var src = v.src;
+							var imgItem = '<img src="'+src+'"/>';
+							$("#imgcont").append(imgItem);
+						});
+
+					}
+
+				});
+
+			});
+		</script>
 	</div>
 </body>
 </html>
